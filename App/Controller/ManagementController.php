a3ec5214b056d0564b3f8066fcbb13adb3559202
Franke266 <ifranjic099@gmail.com>
2023-01-02 21:50:20 +0100

    image upload added

--- App/Controller/ManagementController.php
+++ App/Controller/ManagementController.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 namespace App\Controller;
 
 use App\Model\Management;
-//use App\Validation\MValidator;
 use Core\Input;
 
 class ManagementController extends AbstractController
@@ -18,23 +17,79 @@ class ManagementController extends AbstractController
    
     public function managementAction():void
     {
-        $this->view->render('Management/Management', [
-            'gallery'  => Management::getPhotos(),
+        if ($this->auth->getCurrentUser() === null || !$this->auth->isLoggedIn())
+            {
+                $this->redirect('');
+            }
+        $this->view->render('Management/management', [
+            'gallery'  => Management::getUsers(),
 
         ]);
         
     }
 
-    public function showphotosAction():void
+    public function albumAction($user_id)
     {
-        $res=Management::getPhotosNbr();
+        if ($this->auth->getCurrentUser() === null || !$this->auth->isLoggedIn())
+            {
+                $this->redirect('');
+            }
+        $this->view->render('Management/album', [
+            'gallery2'  => Management::getImages($user_id),
+
+        ]);
+        
+    }
+
+    public function showimagesAction():void
+    {
+        $res=Management::getImagesNbr();
         
         echo json_encode($res);
         
     }
 
-    
+    public function removeImgAction($id)
+    {
+        if ($this->auth->getCurrentUser() === null || !$this->auth->isLoggedIn())
+            {
+                $this->redirect('');
+            }
+        $current_album=\Core\Auth::getInstance()->getCurrentUser()->getid();
+        Management::delete('id', $id);
+        $this->redirect('management/album/'. $current_album);
+    }
 
-   
-    
+    public function addImgAction(){
+        if(isset($_FILES['new-image']) 
+          && $_FILES['new-image']['error'] == UPLOAD_ERR_OK){
+          $info = getimagesize($_FILES['new-image']['tmp_name']);
+          $allowedTypes = [IMAGETYPE_JPEG=>'.jpg',
+                           IMAGETYPE_PNG=>'.png'];
+          if($info === false){ // no go
+            echo "<script>alert('Bad file format!'); 
+            location.href = 'http://localhost/shared-gallery/management';</script>";
+          }else if(!array_key_exists($info[2], $allowedTypes)){ // no go
+            echo "<script>alert('Not an accepted file type!'); 
+            location.href = 'http://localhost/shared-gallery/management';</script>";
+          }else{
+            //save the picture in the images folder
+            $path = getcwd().DIRECTORY_SEPARATOR.'images'.DIRECTORY_SEPARATOR;
+            $filename = uniqid().$allowedTypes[$info[2]];
+            move_uploaded_file($_FILES['new-image']['tmp_name'], $path.$filename);
+            
+            $user_id=\Core\Auth::getInstance()->getCurrentUser()->getid();
+
+            Management::insert([
+                'user_id' => $user_id,
+                'image' => $filename
+            ]);
+            $this->redirect('management');
+         }
+        }else{
+            echo "<script>alert('No file chosen!'); 
+            location.href = 'http://localhost/shared-gallery/management';</script>";
+        }
+      }
+       
 }
